aa51c5bc8dc5f97ff200f4d772690ec62e51ff12
CoderNoOne <firelight.code@gmail.com>
2026-06-01 14:47:52 +0200

    Add admin role check in UserService and implement isAdmin method in User

--- user-service/src/main/java/com/rzodeczko/domain/model/User.java
+++ user-service/src/main/java/com/rzodeczko/domain/model/User.java
@@ -69,6 +69,10 @@ public class User {
         this.role = newRole;
     }
 
+    public boolean isAdmin() {
+        return this.role == Role.ADMIN;
+    }
+
     public UUID getId() {
         return id;
     }
