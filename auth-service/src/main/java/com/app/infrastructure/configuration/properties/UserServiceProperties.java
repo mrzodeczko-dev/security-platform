f0b5dccd6ec5c4f8f4bfe8cefd2820104711a83c
CoderNoOne <firelight.code@gmail.com>
2026-06-03 18:59:37 +0200

    update UserServiceProperties to use Long for timeout values and add JWT import in BeanConfiguration

--- auth-service/src/main/java/com/app/infrastructure/configuration/properties/UserServiceProperties.java
+++ auth-service/src/main/java/com/app/infrastructure/configuration/properties/UserServiceProperties.java
@@ -5,8 +5,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "user-service")
 public record UserServiceProperties(
         String url,
-        String connectTimeoutMs,
-        String readTimeoutMs,
+        Long connectTimeoutMs,
+        Long readTimeoutMs,
         String internalSecret
 
 ) {
